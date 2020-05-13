02c164e5b7767df47e71269f9464a964711eba37
jsonLiang <yuanyu90221@gmail.com>
2020-05-13 23:36:50 +0800

    switch with interface type assert

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yuanyu90221/go_select_switch
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	i = 100
+	convert(i)
+	i = float64(45.55)
+	convert(i)
+	i = "foo"
+	convert(i)
+}
+
+var (
+	i interface{}
+)
+
+func convert(i interface{}) {
+	switch t := i.(type) {
+	case int:
+		println("i is interger", t)
+	case string:
+		println("i is string", t)
+	case float64:
+		println("i is float64", t)
+	}
+}
